27e1094a3db5df59b23e67c469f3947f6bb6d1ca
Manesh Borkar <maneshborkar@Mac.lan>
2025-12-13 13:43:01 +0530

    Initial commit

---
+++ src/main/java/com/springdev/smart_billing_platform/SmartBillingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.springdev.smart_billing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBillingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBillingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdev/smart_billing_platform/SmartBillingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdev.smart_billing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBillingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
